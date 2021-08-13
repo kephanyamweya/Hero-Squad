cff816244880feeeb2bf4304f5fb56cd2d4bf592
kephanyamweya <kephanyamweya95@gmail.com>
2021-08-13 17:29:19 +0300

    add template folder and App.java folder that is responsible for the front-end user interface of my application

---
+++ src/test/java/models/heroesTest.java
@@ -0,0 +1,17 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class heroesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
